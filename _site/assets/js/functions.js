f0c486b94491e461496a6fcc2d6a30d13a634172
arielnunes <arielnunes@hotmail.com>
2015-11-12 17:14:13 -0200

    add

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -90,40 +90,48 @@ function magicScroll(){
   // Init ScrollMagic Controller
 	var controller = new ScrollMagic.Controller();
 	  
-  // Create Animation for 0.5s
-  var tween1 = TweenMax.to('#introAnimation1', 0.3, {
-    backgroundColor: 'rgb(255, 39, 46)',
-    scale: 15,
-  });
-  
-  // Create the Scene and trigger when visible
-  var scene1 = new ScrollMagic.Scene({
-    triggerElement: '#introScene1',
-  })
-
-  .setTween(tween1)
-  .addTo(controller);
-  
-  
-    // Create Animation for 0.5s
-    var tween2 = TweenMax.to('#introAnimation2', 0.3, {
-      backgroundColor: 'rgb(255, 39, 46)',
-      scale: 15,
-    });
-    
-    // Create the Scene and trigger when visible
-    var scene2 = new ScrollMagic.Scene({
-      triggerElement: '#introScene2',
-    })
-  
-    .setTween(tween2)
-    .addTo(controller);
-    
-    
-  
-  // Add debug indicators fixed on right side
-  // scene1.addIndicators();
-  // scene2.addIndicators();
+	var navBlack = new TimelineMax();
+	
+	navBlack.from("#nav-black", 0.1, {opacity: 0, ease:Strong.easeInOut})
+	.to("#nav-black", 0.1, {opacity: 1, ease:Strong.easeInOut})
+	
+	.from("#nav-toggle", 0.1, {opacity: 1, ease:Strong.easeInOut})
+	.to("#nav-toggle", 0.1, {opacity: 0.5, ease:Strong.easeInOut}) 
+		  
+		  
+	// Create Animation for 0.5s
+	var tween1 = TweenMax.to('#nav-black', 0.3, {
+	opacity: 1,
+	});
+	
+	// Create the Scene and trigger when visible
+	var scene1 = new ScrollMagic.Scene({
+	triggerElement: '#introScene2',
+	})
+	
+	.setTween(navBlack)
+	.addTo(controller);
+	  
+	  
+	// Create Animation for 0.5s
+	var tween2 = TweenMax.to('#introAnimation2', 0.3, {
+	backgroundColor: 'rgb(255, 255, 255)',
+	scale: 15,
+	});
+	
+	// Create the Scene and trigger when visible
+	var scene2 = new ScrollMagic.Scene({
+	triggerElement: '#introScene2',
+	})
+	
+	.setTween(tween2)
+	.addTo(controller);
+	
+	
+	
+	// Add debug indicators fixed on right side
+	// scene1.addIndicators();
+	// scene2.addIndicators();
   
 }
 
